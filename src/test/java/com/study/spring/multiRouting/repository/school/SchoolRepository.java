034012cbd1711b77ceb9bf66988cbcd4e47b8e17
youngrankwon <youngran.kwon@gsitm.com>
2023-11-27 17:51:37 +0900

    test code 추가

---
+++ src/test/java/com/study/spring/multiRouting/repository/school/SchoolRepository.java
@@ -0,0 +1,8 @@
+package com.study.spring.multiRouting.repository.school;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SchoolRepository extends JpaRepository<School, Integer> {
+}
